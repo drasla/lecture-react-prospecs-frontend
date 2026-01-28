ed8210c4afda8ca532bbf325f735be57ca950eda
TaeGaram (Office) <drasla217@gmail.com>
2026-01-28 12:44:06 +0900

    PaymentSuccess, PaymentFail 페이지 완성

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -8,6 +8,8 @@ import ProductListPage from "../pages/(shop)/ProductListPage.tsx";
 import ProductDetailPage from "../pages/(shop)/ProductDetailPage.tsx";
 import CartPage from "../pages/(shop)/CartPage.tsx";
 import OrderPage from "../pages/(shop)/OrderPage.tsx";
+import OrderSuccessPage from "../pages/(shop)/OrderSuccessPage.tsx";
+import OrderFailPage from "../pages/(shop)/OrderFailPage.tsx";
 
 // loader : 해당 주소에 사용자가 가려고 할 때 (요청), 화면에 출력해주기 이전 실행되는 함수를 지정
 //           조건을 걸고, 아무런 문제가 없으면 null을 반환해서 화면이 출력이 되도록 해야함
@@ -30,7 +32,14 @@ const router = createBrowserRouter([
             { path: "login", element: <Login />, loader: guestOnlyLoader },
             { path: "register", element: <Register />, loader: guestOnlyLoader },
             { path: "cart", element: <CartPage /> },
-            { path: "order", element: <OrderPage /> },
+            {
+                path: "order",
+                children: [
+                    { index: true, element: <OrderPage /> },
+                    { path: "success", element: <OrderSuccessPage /> },
+                    { path: "fail", element: <OrderFailPage /> },
+                ],
+            },
             { path: "category/:id", element: <ProductListPage /> },
             { path: "product/:id", element: <ProductDetailPage /> },
         ],
